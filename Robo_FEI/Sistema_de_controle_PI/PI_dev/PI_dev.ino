4867b07be918f5fcaaf7fb1e28e8a1cbc566f02c
RoboFEIatWork <robofei.atwork@gmail.com>
2024-10-10 15:41:29 -0300

    aaaaa

--- Robo_FEI/Sistema_de_controle_PI/PI_dev/PI_dev.ino
+++ Robo_FEI/Sistema_de_controle_PI/PI_dev/PI_dev.ino
@@ -15,11 +15,8 @@ BluetoothSerial SerialBT;
 
 // Define os pinos utilizados para a leitura dos sinais do encoder
 //motor superior esquerdo
-#define dev_FE_A 33
-#define dev_FE_B 25
-
-//#define Pino_FE_A 33
-//#define Pino_FE_B 25
+#define Pino_FE_A 33
+#define Pino_FE_B 25
 #define Pino_FE_PWM 32
 //motor superior direito
 #define Pino_FD_A 23
@@ -37,6 +34,7 @@ BluetoothSerial SerialBT;
 #define indicador_PI 14
 
 //definição dos encoders
+ESP32Encoder encoder_FE;//frontal esquerdo(0)
 ESP32Encoder encoder_FD;//frontal direito(1)
 ESP32Encoder encoder_TE;//trás esquerdo(2)
 ESP32Encoder encoder_TD;//trás direito(3)
@@ -58,7 +56,7 @@ int goal_rpm = 0;
 
 void get_rpm(){
   get_encoder();
-  msegundos_ini = millis();
+  msegundos_ini = millis()
   while (millis()-msegundos_ini < tempo_contagem) {
   }
   get_encoder();
@@ -70,28 +68,96 @@ void get_rpm(){
 
 void get_encoder(){
   // encoders vao de 0-1023
-
+  encoder_posicao_anterior[0] = encoder_posicao[0];
   encoder_posicao_anterior[1] = encoder_posicao[1];
   encoder_posicao_anterior[2] = encoder_posicao[2];
   encoder_posicao_anterior[3] = encoder_posicao[3];
 
-  encoder_posicao[1] = encoder_FD.getCount();
-  encoder_posicao[2] = encoder_TE.getCount();
-  encoder_posicao[3] = encoder_TD.getCount();
+  encoder_posicao[0] = encoder_FE.getCount()
+  encoder_posicao[1] = encoder_FD.getCount()
+  encoder_posicao[2] = encoder_TE.getCount()
+  encoder_posicao[3] = encoder_TD.getCount()
 }
 
 
+void message_handler(){
+  mensagem = SerialBT.readString(); // a string mensagem armazena o que foi enviado pelo bluetooth para o arduino
+  goal_rpm = (((double)mensagem[1]-48)*10+((double)mensagem[2]-48));
+  //mover para frente
+  if(mensagem[0]=='f'){
+    rpm_desejado[0]=-1*goal_rpm;
+    rpm_desejado[1]=-1*rpm_desejado[0];
+    rpm_desejado[2]=rpm_desejado[0];
+    rpm_desejado[3]=-1*rpm_desejado[0];
+  }
+  //mover para trás
+  if(mensagem[0]=='t'){
+    rpm_desejado[0]=goal_rpm;
+    rpm_desejado[1]=-1*rpm_desejado[0];
+    rpm_desejado[2]=rpm_desejado[0];
+    rpm_desejado[3]=-1*rpm_desejado[0];
+  }
+  //girar direita
+  if(mensagem[0]=='w'){
+    rpm_desejado[0]=goal_rpm;
+    rpm_desejado[2]=0;
+    rpm_desejado[1]=rpm_desejado[0];
+    rpm_desejado[3]=0;
+  }
+  //girar esquerda
+  if(mensagem[0]=='q'){
+    rpm_desejado[0]=-1*goal_rpm;
+    rpm_desejado[2]=0;
+    rpm_desejado[1]=rpm_desejado[0];
+    rpm_desejado[3]=0;
+  }
+  //mover para direita
+  if(mensagem[0]=='d'){
+    rpm_desejado[0]=goal_rpm;
+    rpm_desejado[2]=-1*rpm_desejado[0];
+    rpm_desejado[1]=rpm_desejado[0];
+    rpm_desejado[3]=-1*rpm_desejado[0];
+  }
+  //mover para esquerda
+  if(mensagem[0]=='e'){
+    rpm_desejado[0]=-1*goal_rpm;
+    rpm_desejado[2]=-1*rpm_desejado[0];
+    rpm_desejado[1]=rpm_desejado[0];
+    rpm_desejado[3]=-1*rpm_desejado[0];
+  }
+  if(mensagem[0]=='p'){
+    rpm_desejado[0]=0;rpm_desejado[1]=0;rpm_desejado[2]=0;rpm_desejado[3]=0;
+    pwm[0]=512;pwm[1]=512;pwm[2]=512;pwm[3]=512;
+    rpm[0]=0;rpm[1]=0;rpm[2]=0;rpm[3]=0;
+    ledcWrite(Pino_FE_PWM, pwm[0]);
+    ledcWrite(Pino_FD_PWM, pwm[1]);
+    ledcWrite(Pino_TE_PWM, pwm[2]);
+    ledcWrite(Pino_TD_PWM, pwm[3]); 
+  }
+}
+
+void send_info_BT(){
+    get_rpm();
+    SerialBT.print("RPM Desejado: ");
+    SerialBT.print(rpm[0]);
+    SerialBT.println("");
+    SerialBT.print("encoder_posicao: ");
+    SerialBT.print(encoder_posicao[0]); 
+    SerialBT.println("");
+}
+
 void setup() {
   Serial.begin(9600);
+  SerialBT.begin(9600); //Bluetooth device name
 
-  //dev_FE
-  pinMode(dev_FE_A, OUTPUT);
-  pinMode(dev_FE_B, OUTPUT);
+  //encoder frontal esquerdo
+  pinMode(Pino_FE_A, INPUT_PULLUP);
+  pinMode(Pino_FE_B, INPUT_PULLUP);
+  encoder_FE.attachHalfQuad(Pino_FE_A, Pino_FE_B);
   //pwm frontal esquerdo
   pinMode(Pino_FE_PWM, OUTPUT);
   ledcAttachChannel(Pino_FE_PWM, 500, 10, 0);
   ledcWrite(Pino_FE_PWM, pwm[0]);
-  ledcWrite(dev_FE_A, pwm[0]);
 
   //encoder frontal direito
   pinMode(Pino_FD_A, INPUT_PULLUP);
@@ -123,34 +189,29 @@ void setup() {
 
 // A função de loop é executada repetidamente para sempre
 void loop() {
+  // Se uma mensagem foi recebida por BlueTooth chama a funcao 'menssage_handler'
+  if (SerialBT.available()){
+    message_handler();
+  }
+
+
   if(contando==1 && millis()-msegundos_ini > tempo_contagem){
-    get_rpm();
-    Serial.print("RPM Desejado: ");
-    Serial.print(rpm[1]);
-    Serial.println("");
-    Serial.print("encoder_posicao: ");
-    Serial.print(encoder_posicao[1]); 
-    Serial.println("");
+    send_info_BT();
     contando=0;
   }
 
-  pwm[0]=600;
-  pwm[1]=600;
-  pwm[2]=600;
-  pwm[3]=600;
-
   // Atualiza os PWMs
-  //for (int i=0; i<4; i++){
-  //  if (rpm_desejado[i]>rpm[i] and contando==0){
-  //    pwm[i]=pwm[i]+1;
-  //  }
-  //  if(rpm_desejado[i]<rpm[i] and contando==0){
-  //    pwm[i]=pwm[i]-1;
-  //  }   
-  //  if (rpm_desejado[i]==0){pwm[i]=512;}
-  //}
+  for (int i=0; i<4; i++){
+    if (rpm_desejado[i]>rpm[i] and contando==0){
+      pwm[i]=pwm[i]+1;
+    }
+    if(rpm_desejado[i]<rpm[i] and contando==0){
+      pwm[i]=pwm[i]-1;
+    }   
+    if (rpm_desejado[i]==0){pwm[i]=512;}
+  }
 
-  ledcWrite(dev_FE_A, pwm[0]);
+
   ledcWrite(Pino_FE_PWM, pwm[0]);
   ledcWrite(Pino_FD_PWM, pwm[1]);
   ledcWrite(Pino_TE_PWM, pwm[2]);
